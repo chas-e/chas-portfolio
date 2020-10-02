5c1030ee7c1f5a4e8310eaa4de8c5bea1941dd6a
chas-e <sigmapi76388@gmail.com>
2020-10-02 17:12:49 -0400

    adding google analytics & config

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-179490103-1`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
