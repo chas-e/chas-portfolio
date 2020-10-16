46b3f35065c5d7e44627edfcd161ed4d47aac54c
chas-e <sigmapi76388@gmail.com>
2020-10-16 13:01:01 -0400

    adjusting styles

--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,7 @@ const Header = ({ siteTitle }) => (
       backgroundColor: `RGBA(252, 252, 252, .85)`,
       marginBottom: `3vmin`,
       width: `100%`,
-      maxHeight: `15vh`,
+      maxHeight: `10vh`,
       position: `fixed`,
       padding: `3vmin`,
     }}
